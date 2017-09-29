6ef0c3617aaa052461e480f5fcdea470269ff82f
supachok654 <dome654@hotmail.co.th>
2017-09-29 16:02:40 +0700

    This is space created

---
+++ space.py
@@ -0,0 +1,18 @@
+import arcade
+from random import randint
+ 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+class AlienWindow(arcade.Window):
+    def __init__(self,width,height):
+        super().__init__(width, height)
+ 
+        arcade.set_background_color(arcade.color.BLACK)
+ 
+def main():
+    window = AlienWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.set_window(window)
+    arcade.run()
+ 
+if __name__ == '__main__':
+    main()      
