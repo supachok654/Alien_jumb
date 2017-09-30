c385eb62f18cdb67de8f2baa4fb097b125d46899
supachok654 <dome654@hotmail.co.th>
2017-09-30 17:25:11 +0700

    created alien

--- space.py
+++ space.py
@@ -7,6 +7,8 @@ class AlienWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width, height)
         self.background = None
+        self.alien = arcade.Sprite('images/alien1.png')
+        self.alien.set_position(400, 125)
         
     def setup(self):
         self.background = arcade.load_texture("images/space2.jpg")
@@ -14,6 +16,7 @@ class AlienWindow(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.alien.draw()
 def main():
     window = AlienWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     window.setup()
