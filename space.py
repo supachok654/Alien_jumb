532ee6ef0defb2a7a3a9d8b8a6f7576d020d07e1
supachok654 <dome654@hotmail.co.th>
2017-09-30 17:11:07 +0700

    set background

--- space.py
+++ space.py
@@ -1,16 +1,22 @@
 import arcade
 from random import randint
  
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 1000
 class AlienWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width, height)
- 
-        arcade.set_background_color(arcade.color.BLACK)
- 
+        self.background = None
+        
+    def setup(self):
+        self.background = arcade.load_texture("images/space2.jpg")
+    
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 def main():
     window = AlienWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.setup()
     arcade.set_window(window)
     arcade.run()
  
