cba9d8e61e9806522c16ec612abcf848aec5b6e2
supachok654 <dome654@hotmail.co.th>
2017-10-03 23:55:51 +0700

    created base

--- models.py
+++ models.py
@@ -10,11 +10,24 @@ class Alien:
             self.x = 0
         if self.x < 0:
             self.x = 800
+class Base:
+    def __init__(self,world,x,y):
+        self.world = world
+        self.x = x
+        self.y = y
+    def update(self,delta):
+        if self.x > self.world.width:
+            self.x = 0
+        if self.x < 0:
+            self.x = 800
+        
 class World:
     def __init__(self,width,height):
         self.width = width
         self.height = height
-
         self.alien = Alien(self,400,800)
+        self.base = Base(self,400,500)
     def update(self,delta):
-        self.alien.update(delta)        
+        self.alien.update(delta)
+        self.base.update(delta)
+
--- space.py
+++ space.py
@@ -1,10 +1,12 @@
 import arcade
-from models import World,Alien
+from models import World,Alien,Base
 from random import randint
  
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 1000
-GRAVITY_CONSTANT = 750
+GRAVITY_CONSTANT = 750 #เดี๋ยวมาแก้ ความโน้มถ่วงให้เพิ่มตามเวลาที่เปลี่ยนไป 
+                       #โดยสร้างตัวแปรเวลามาเก็บ
+                       #แล้วมาคูนกับ gravity
 
 class AlienWindow(arcade.Window):
     def __init__(self,width,height):
@@ -12,9 +14,9 @@ class AlienWindow(arcade.Window):
         self.background = None
         self.world = World(width,height)
         self.alien_sprite = arcade.Sprite('images/alien1.png')
+        self.base_sprite = arcade.Sprite('images/base_1.png')
         #self.alien.set_position(400, 125) #alien position
-        #self.playerx = 400
-        #self.playery = 850
+        
 
     def setup(self):
         self.background = arcade.load_texture("images/space2.jpg")
@@ -23,9 +25,11 @@ class AlienWindow(arcade.Window):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.alien_sprite.draw()
+        self.base_sprite.draw()
     def update(self,delta):
         self.world.update(delta)
         self.alien_sprite.set_position(self.world.alien.x,self.world.alien.y)
+        self.base_sprite.set_position(self.world.base.x,self.world.base.y)
         self.world.alien.y -= GRAVITY_CONSTANT*delta
         print(self.world.alien.y)
 def main():
