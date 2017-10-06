f66b9213ee4578ce79c12b9e6ba77849c3f73416
supachok654 <dome654@hotmail.co.th>
2017-10-06 11:43:37 +0700

    more base

--- models.py
+++ models.py
@@ -1,40 +1,61 @@
 import arcade
+from random import randint
 GRAVITY_CONSTANT = 750
-class Alien:
+NUM_BASE = 5
+
+class Alien(arcade.Sprite):
     def __init__(self,world,x,y):
+        super().__init__('images/alien1.png')
         self.world = world
-        self.x = x
-        self.y = y
-        self.delta_x = 0
-        self.delta_y = GRAVITY_CONSTANT
+        self.center_x = x
+        self.center_y = y
+        self.change_x = 0
+        self.chnage_y = GRAVITY_CONSTANT
+
     def update(self,delta):
-        if self.x > self.world.width:
-            self.x = 0
-        if self.x < 0:
-            self.x = 800
-        if self.y < 0:
-            self.y = self.world.height
-class Base:
+        super().update()
+        if self.center_x > self.world.width:
+            self.center_x = 0
+        if self.center_x < 0:
+            self.center_x = 800
+        if self.center_y < 0:
+            self.center_y = self.world.height
+
+class Base(arcade.Sprite):
     def __init__(self,world,x,y):
+        super().__init__('images/base_3.png')
         self.world = world
-        self.x = x
-        self.y = y
+        self.center_x = x
+        self.center_y = y
+        self.change_y = -1
+
     def update(self,delta):
-        if self.y > self.world.height:
-            self.y = 0
-        if self.y < 0:
-            self.y = 1000
-        if self.x > self.world.width:
-            self.x = 0
-        if self.x < 0:
-            self.x = 800
+        super().update()
+        if self.center_y > self.world.height:
+            self.center_y = 0
+        if self.center_y < 0:
+            self.center_y = 1000
+            self.center_x = randint(100,700)
+        if self.center_x > self.world.width:
+            self.center_x = 0
+        if self.center_x < 0:
+            self.center_x = 800
+
 class World:
     def __init__(self,width,height):
         self.width = width
         self.height = height
-        self.alien = Alien(self,400,800)
-        self.base = Base(self,400,200)
+        self.alien = Alien(self, 400, 800)
+        #self.base = Base(self,randint(100,700),randint(200,1000))
+        self.base_list = arcade.SpriteList()
+        for x in range(NUM_BASE):
+            self.base_list.append(Base(self, randint(100,700),randint(200,1000)))
+        
     def update(self,delta):
         self.alien.update(delta)
-        self.base.update(delta)
+        #self.base.update(delta)
+        #self.base = Base(self,randint(100,700),randint(200,1000))
+        #self.base_list.append(Base(self,randint(100,700),randint(200,1000)))
+        for base in self.base_list:
+            base.update(delta)
 
--- space.py
+++ space.py
@@ -12,50 +12,65 @@ MOVEMENT_CONSTANT = 5
 BASE_CONSTANT_Y = 500
 BASE_CONSTANT_X = 500
 BOUNCINESS = 5
-
 class AlienWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width, height)
         self.background = None
         self.base_list = None 
         self.world = World(width,height) 
-        self.alien_sprite = arcade.Sprite('images/alien1.png')
-        self.base_sprite = arcade.Sprite('images/base_3.png')
+        #self.alien_sprite = arcade.Sprite('images/alien1.png')
+        #self.base_sprite = arcade.Sprite('images/base_3.png')
         alien = Alien(self.world,SCREEN_WIDTH,SCREEN_HEIGHT)
-        self.delta_x = alien.delta_x
-        self.delta_y = alien.delta_y
-        
+        #self.delta_x = alien.delta_x
+        #self.delta_y = alien.delta_y
+        self.delta_x = alien.change_x
+        self.delta_y = alien.chnage_y
+
         #self.alien.set_position(400, 125) #alien position
         
 
     def setup(self):
         self.background = arcade.load_texture("images/space2.jpg")
         self.base_list = arcade.SpriteList()
-        self.alien_sprite.center_x = self.world.alien.x
-        self.alien_sprite.center_y = self.world.alien.y
+        #self.alien_sprite.center_x = self.world.alien.x
+        #self.alien_sprite.center_y = self.world.alien.y
     
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        self.alien_sprite.draw()
-        self.base_sprite.draw()
+
+        self.world.alien.draw()
+
+        for base in self.world.base_list:
+            base.draw()
 
     def update(self,delta):
         self.world.update(delta)
-        self.alien_sprite.set_position(self.world.alien.x,self.world.alien.y)
-        self.base_sprite.set_position(self.world.base.x,self.world.base.y)  
-        self.world.alien.y -= self.delta_y*delta
-        self.world.alien.x += self.delta_x
-        self.world.base.y -= BASE_CONSTANT_Y*delta
-        #self.world.base.x -= BASE_CONSTANT_X*delta
-        print(self.world.alien.y)
+        #self.alien_sprite.set_position(self.world.alien.x,self.world.alien.y)
+        #self.base_sprite.set_position(self.world.base.x,self.world.base.y)  
+        #self.world.alien.y -= self.delta_y*delta
+        #self.world.alien.x += self.delta_x
+        #self.world.base.y -= BASE_CONSTANT_Y*delta
+        #self.world.base.x -= BASE_CONSTANT_X*delta #ถามเพื่อนว่าควรมีตรงนี้มั้ย
+        print(self.world.alien.center_y)
         #print(self.alien_sprite.center_y)
         #print(self.world.base.y)
+        self.world.alien.change_x = self.delta_x
+        self.world.alien.change_y = -2
+        for base in self.world.base_list:
+            if(self.world.alien.center_y >= base.center_y - 10.5 and self.world.alien.center_y <= base.center_y + 50.5 \
+               and self.world.alien.center_x <= base.center_x + 65 and self.world.alien.center_x >= base.center_x - 65):
+                #self.delta_y *= -BOUNCINESS #แก้
+                self.world.alien.change_y = 0
+
+        '''
         if(self.alien_sprite.center_y >= self.world.base.y-10.5 and self.alien_sprite.center_y <= self.world.base.y+50.5  and self.alien_sprite.center_x <= self.world.base.x+65 and self.alien_sprite.center_x >= self.world.base.x-65):
-                self.delta_y *= -BOUNCINESS
+                #self.delta_y *= -BOUNCINESS #แก้
+                self.delta_y = 0
                 
         else:
             self.delta_y = GRAVITY_CONSTANT
+        '''
         
 
     def on_key_press(self,key,modifiers):
